9227560cab28185334a80829ecb7c100f73724be
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-05-06 10:24:10 -0300

    aa

---
+++ Paginas aletorios que fiz/JAVASCRIPT/Storage/AULA02-02/aula02.js
@@ -0,0 +1,7 @@
+let nome ='Bruno'
+console.log('cfb cursos')
+// impressão disso
+console.log(nome)
+console.log('Nome: ' + nome)
+
+// quando vc usa o modo restrito, vc precisa definir, é tipo exigir definir a var
