8a9d5fba5cd053b86276a15d5604731436aa9a30
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-05-15 12:21:25 -0300

    ATUALIZAR

---
+++ Programação WEB aulas/Aula03/aula04PHP/aula04.php
@@ -0,0 +1,32 @@
+<?php
+
+$idade="18";
+print "<p> Minha idade é $idade.</p>"
+?>
+<?php 
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo PHP</title>
+</head>
+<body>
+
+    <h1>Bem-vindo!</h1>
+
+    <?php
+    $nome = "João";
+    $idade = 30;
+    ?>
+
+    <p>Olá, meu nome é <?php echo $nome; ?> e tenho <?php echo $idade; ?> anos.</p>
+
+    <?php
+    
+    ?>
+
+</body>
+</html>
