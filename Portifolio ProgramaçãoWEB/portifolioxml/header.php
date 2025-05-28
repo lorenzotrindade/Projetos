6e51f3bb82c82f1bbb33b28271d528e85a6dd49e
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-05-27 22:57:14 -0300

    aaaaa

---
+++ Portifolio ProgramaçãoWEB/portifolioxml/header.php
@@ -0,0 +1,13 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Descubra seu Signo</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+</head>
+<body class="container mt-5">
+
