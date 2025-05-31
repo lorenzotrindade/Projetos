71fddedbf06bef614361df9887d76a42b6acbc52
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-05-31 12:47:47 -0300

    aaaa

---
+++ CFB CURSOS/JS/OP TERNÁRIO/op perg1.js
@@ -0,0 +1,9 @@
+let num  = 10
+let num2= 5
+res=(num > num2 ? "Vasco" : "palmeiras, pois a op foi falsa em relação a compáração")
+console.log(res)
+
+// 0 = false
+// 1= true
+
+// teste logico ? se verdadeiro : se  falso
