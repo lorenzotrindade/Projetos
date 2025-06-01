7be583aafad27c551e82a3b320b23cd240b5a5ae
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-06-01 11:43:43 -0300

    aulas

---
+++ CFB CURSOS/JS/typeofaula11/aula11.js
@@ -0,0 +1,12 @@
+let v1,v2,v3,v4
+v1=10 
+v2="10"
+v3=v1===v2
+v4={nome:'Bruno'}
+
+// é tudo diferente no typo
+
+console.log("Valor: " + v1 + " - Tipo: " + typeof (v1))
+console.log("Valor: " + v2 + " - Tipo: " + typeof (v2))
+console.log("Valor: " + v3 + " - Tipo: " + typeof (v3))
+console.log("Valor: " + v4 + " - Tipo: " + typeof (v4))
