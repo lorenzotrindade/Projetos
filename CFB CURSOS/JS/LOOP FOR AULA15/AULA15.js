0e9ec53bb2d7ee3f9f306ba99ac7bcd34f90cbb5
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-06-02 23:39:19 -0300

    aulas hj

---
+++ CFB CURSOS/JS/LOOP FOR AULA15/AULA15.js
@@ -0,0 +1,9 @@
+console.log("inicio programa")
+
+for(let i =0;i<10;i++) {
+    console.log("lorenzo mais "+i)
+}
+
+console.log("fim do programa")
+
+// nos usamos o for quando temos uma noção de até que ponto iremos repetir aquela operação, já o while e do while não
---
+++ CFB CURSOS/JS/aula14 SWITCH CASE/AULA14.js
@@ -0,0 +1,26 @@
+let colocacao = 5
+
+switch(colocacao) {
+
+    case 1: 
+    console.log('Primeiro Lugar')
+    break
+    
+    case 2: 
+    console.log("segundo lugar")
+    break
+
+    case 3: 
+    console.log ("terceiro Lugar")
+    break
+
+    case 4: case 5: case 6:  
+    console.log('Premio de participação')
+    break
+
+    default:
+        console.log("Não subiu ao podio")
+
+}
+
+// que nem o if, se a variavel (parametro), não sustentar nada, então nem, vai entrar em nem um daqueles casos
