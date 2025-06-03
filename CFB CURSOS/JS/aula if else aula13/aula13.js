c44d1fba420a9fd2541608e092b312b4792db757
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-06-02 21:48:54 -0300

    aaaa

---
+++ CFB CURSOS/JS/aula if else aula13/aula13.js
@@ -0,0 +1,14 @@
+let num = 10
+
+if( num > 10) {
+    console.log("Numeral maior que 10")
+    
+
+} else  if (num> 5){
+    console.log("numeral menor que 10")
+
+} else {
+    
+}
+
+console.log("fim do programa")
