663ce8a01d87543d89741d9820ea18ad75451746
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-06-02 22:29:22 -0300

    aaaa

--- CFB CURSOS/JS/aula if else aula13/aula13.js
+++ CFB CURSOS/JS/aula if else aula13/aula13.js
@@ -4,11 +4,13 @@ if( num > 10) {
     console.log("Numeral maior que 10")
     
 
-} else  if (num> 5){
+} else  if (num > 5){
     console.log("numeral menor que 10")
 
 } else {
+    console.log("fim do programa")
     
 }
 
-console.log("fim do programa")
+
+// desta forma podemos fazer varios else ifs um atrás do outro
---
+++ CFB CURSOS/JS/aula if else aula13/aula13aninhando.js
@@ -0,0 +1,20 @@
+// é quando tem um if dentro de outro
+
+let num = 10
+
+if( num > 10) {
+    console.log("Numeral maior que 10")
+
+    if (num>50) {
+        console.log("numeral maior q 50")
+
+    }
+    
+
+} else if(num >5){
+    console.log("numeral esta entre 6 e 10")
+
+
+} else {
+    console.log("numeral menor ou igual a 5")
+}
