dbe2bf871d261a23108a76db05317aefdfaae5ae
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-06-06 07:40:13 -0300

    aaa

---
+++ CFB CURSOS/JS/ARROW FUNCTION/aula25A.js
@@ -0,0 +1,6 @@
+
+const soma=(v1,v2)=>{return v1+v2}
+const nome=n=>{return n}
+
+console.log(soma(10,5))
+console.log(soma("bruno"))
---
+++ CFB CURSOS/JS/ARROW FUNCTION/aula25B.js
@@ -0,0 +1,7 @@
+const add=n=>n+10
+// add = n => ...: Define uma função chamada add. Essa é uma arrow function (função de seta), uma sintaxe mais concisa para definir funções em JavaScript. n é o parâmetro de entrada da função.
+
+
+console.log(add(10))
+
+
