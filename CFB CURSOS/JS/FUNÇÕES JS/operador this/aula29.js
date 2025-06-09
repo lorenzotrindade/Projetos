2d90ade78feb085f9694ec65f7e44a9fd4c44a5b
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-06-08 23:24:58 -0300

    aaaaaaa

---
+++ CFB CURSOS/JS/FUNÇÕES JS/operador this/aula29.js
@@ -0,0 +1,11 @@
+function aluno(nome,nota) {
+     this.nome=nome
+     // to dizendo que nome pertence e a aluno e recebe nom
+     
+     this.nota=nota
+     console.log(nome)
+     console.log(nota)
+
+     aluno('bruno' ,100)
+
+}
---
+++ CFB CURSOS/JS/METODOS DOM/GEYBYID/AULA30.js
@@ -0,0 +1,20 @@
+const dc1 =document.getElementById("c1")
+console.log(dc1)
+// vamos conseguir tudo que queremos desse elemento aprte por partes.
+
+console.log(dc1.id)
+console.log(dc1.innerHTML)
+dc1.innerHTML='lorenzo'
+//pegando o elemento e trocando ele
+
+
+const dc2=document.getElementById('c2')
+const dc3=document.getElementById('c3')
+const dc4=document.getElementById('c4')
+const dc5=document.getElementById('c5')
+const dc6=document.getElementById('c6')
+// criando umarray delas
+
+const arrayElementos=[dc1,dc2,dc3,dc4,dc5,dc6]
+console.log(arrayElementos)
+// imprimindo todos os elementos dele
