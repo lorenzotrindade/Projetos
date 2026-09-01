5e01903d06b6448f476dd6f3c8fa2ef76d0266ed
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2026-09-01 20:35:29 -0300

    a

---
+++ c or c#/10agosto/aula3_exe4.c
@@ -0,0 +1,19 @@
+/*
+4. Escreva um programa que leia um número de dois dígitos e subtraia o valor de cada um dos
+dígitos do valor lido. O valor resultante deve ser exibido.
+
+*/
+#include <stdio.h>
+
+main() {
+    int n, d, un, r;
+    
+    printf("Digite um numero de dois digitos (10 a 99): ");
+    scanf("%d", &n);   
+    d = n / 10;   
+    un = n % 10;  
+
+    r = n - d - un;
+
+    printf("O valor resultante eh: %d\n", r);
+}
