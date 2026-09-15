8e0aaf969249f3fddeaee5bbfda48813e6b3253a
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2026-09-15 00:08:57 -0300

    a

--- c or c#/simulado/codigo4.c
+++ c or c#/simulado/codigo4.c
@@ -3,6 +3,8 @@ e outro valor inteiro M , e verifique se o digito q representa N
  na notação decimal está ebtre os digitos da re´presentação do 
  valor M*/
 
+
+
 #include <stdio.h>
 
 int main() {
@@ -21,3 +23,5 @@ int main() {
     // não entendi a parte de representação decimal.. se pede  de 0 a 9 indcluse 0 e 9
     // claro não me de a resposta.. me ajude a desenvplver o racicionio ´para chegar a copnclusão correta.. e claro seguindo o meio de pensamento e metodos q da pra utiliz\ar em C e usando aqulas fontes de github q mandei..
 }
+
+
--- c or c#/simulado/codigo4v2.c
+++ c or c#/simulado/codigo4v2.c
@@ -22,3 +22,39 @@ int main() {
     }
 }
 // cara sinto q ta faltando algo.. ajude a desenvpolevr meu raciocioneo mas semd as a resposta..
+
+ /*
+ Para escrever esse codigo tenho q ter 3 coisas em emnte
+ quem controla meu laço?
+ o que comparar no if?
+ como o numero m andar para o proximo digito?
+ 
+ */
+
+ //v2
+ #include <stdio.h>
+
+int main() {
+    int n, m,i,r;
+
+    printf("digite um valor entre 0 e 9");
+    scanf("%d",&n);
+    printf(" digite o valor inteiro:");
+    scanf("%d",&m);
+    
+    //'\0'n indica o fim de uma string apenas
+
+    while(m > 0){
+        // estamos em loop infinito, pq não guardamos a variavel
+        i = m/10;
+        r = m%10;
+        m=i;
+
+            // com quem eu devo comparar?
+            if(n==r){
+                printf("Achou");
+                m=0;
+            } 
+        }
+            printf("não achou");     
+    }
